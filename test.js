a8bf39b11fc1d2f05eece2bfa24608544ac2a142
pengcl <3559996@qq.com>
2020-10-23 15:21:23 +0800

    接入供应商接口

--- test.js
+++ test.js
@@ -2,31 +2,34 @@ const items = [
   { srNo: '1.1' },
   { srNo: '1.1.1' },
   { srNo: '1.1.2' },
+  { srNo: '1.12.1' },
   { srNo: '1.2' },
   { srNo: '1.2.2' },
   { srNo: '1.2.1' },
+  { srNo: '1.21.1' },
   { srNo: '1.3' },
   { srNo: '1.3.1' },
-  { srNo: '1.3.2' }
+  { srNo: '1.3.2' },
+  { srNo: '2.1' },
+  { srNo: '2.1.1' }
 ]
 
 function toNum (a) {
   a = a.toString()
   const c = a.split('.')
-  const r = ['', '0', '00', '000', '0000'].reverse()
-  for (let i = 0; i < c.length; i++) {
-    const len = c[i].length
-    c[i] = r[len] + c[i]
-  }
   return c.join('')
 }
 
 function add (str) {
-  const filterItems = items.filter(item => item.srNo.indexOf(str) === 0).sort((a, b) => b.srNo - a.srNo)
+  const filterItems = str ? items.filter(item => item.srNo.indexOf(str) === 0).sort((a, b) => b.srNo - a.srNo) : items
   filterItems.sort((a, b) => {
     return toNum(b.srNo) - toNum(a.srNo)
   })
   console.log(filterItems)
+  const arr = str ? filterItems[0].srNo.split('.').reverse() : filterItems[0].srNo.split('.')
+  arr[0] = parseInt(arr[0], 10) + 1 + ''
+  return arr.reverse()
+  // console.log(parseInt(filterItems[0].split('.').reverse()[0],10) + 1)
 }
 
-add('1.2')
+add(0)
