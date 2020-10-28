f140686b7520a3cc426057f7e787cf1d902dd5f8
pengcl <3559996@qq.com>
2020-10-28 12:00:31 +0800

    添加项目及合同接口

--- src/views/contract/components/ContractList.vue
+++ src/views/contract/components/ContractList.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-form :form="form" :label-col="{ span: 8 }" :wrapper-col="{ span: 16 }">
+  <a-form :label-col="{ span: 8 }" :wrapper-col="{ span: 16 }">
     <a-row :gutter="48">
       <a-col :md="12" :sm="24">
         <a-button type="success">按编码排序</a-button>
@@ -9,102 +9,180 @@
         <a-button type="success" style="float: right">导入导出</a-button>
       </a-col>
       <a-col :md="24" :sm="24">
-        <table>
-          <thead>
-            <tr>
-              <th colspan="25">
-                <a-button @click="add()" :disabled="type === 'view'" icon="plus">
-                  新增
-                </a-button>
-                <a-button @click="clear()" :disabled="type === 'view'" icon="stop">
-                  重置
-                </a-button>
-              </th>
-            </tr>
-            <tr>
-              <th rowspan="2">操作</th>
-              <th rowspan="2">
-                带数项目
-              </th>
-              <th style="width: 60px" rowspan="2">清单编号</th>
-              <th style="width: 60px" rowspan="2">标段</th>
-              <th style="width: 5%" rowspan="2">楼栋</th>
-              <th style="width: 5%" rowspan="2">分部</th>
-              <th style="width: 5%" rowspan="2">分项</th>
-              <th style="width: 5%" rowspan="2">预留字段0</th>
-              <th style="width: 5%" rowspan="2">预留字段1</th>
-              <th style="width: 5%" rowspan="2">清单描述</th>
-              <th style="width: 5%" rowspan="2">业态成本中心</th>
-              <th style="width: 5%" rowspan="2">清单项类别</th>
-              <th style="width: 15%" colspan="4">供应</th>
-              <th style="width: 15%" colspan="4">安装</th>
-              <th style="width: 15%" colspan="4">供应+安装</th>
-              <th style="width: 5%" rowspan="2">合计</th>
-            </tr>
-            <tr>
-              <th>单位</th>
-              <th>工程量</th>
-              <th>单价</th>
-              <th>小计</th>
-              <th>单位</th>
-              <th>工程量</th>
-              <th>单价</th>
-              <th>小计</th>
-              <th>单位</th>
-              <th>工程量</th>
-              <th>单价</th>
-              <th>小计</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-if="!item.isDeleted" v-for="(item,index) in data.contractBQlst" :key="item.id">
-              <td>
-                <div style="width: 220px;">
-                  <a-button @click="add(item.srNo)" :disabled="type === 'view'" icon="plus">
-                    添加子项
+        <div class="table-wrapper">
+          <table>
+            <thead>
+              <tr>
+                <th colspan="25">
+                  <a-button @click="add()" :disabled="type === 'view'" icon="plus">
+                    新增
                   </a-button>
-                  <a-button @click="del(index)" :disabled="type === 'view'" icon="close">
-                    删除
+                  <a-button @click="clear()" :disabled="type === 'view'" icon="stop">
+                    重置
                   </a-button>
-                </div>
-              </td>
-              <td>
-                <a-icon
-                  @click="item.isCarryData = !item.isCarryData"
-                  :type="item.isCarryData ? 'check-square' : 'border'"/>
-              </td>
-              <td><a-input :disabled="true" :value="item.srNo"></a-input></td>
-              <td><a-input v-model="item.section"></a-input></td>
-              <td><a-input v-model="item.building"></a-input></td>
-              <td></td>
-              <td></td>
-              <td><a-input v-model="item.remark1"></a-input></td>
-              <td><a-input v-model="item.remark2"></a-input></td>
-              <td><a-input v-model="item.description"></a-input></td>
-              <td><!--{{ item.costCenter }}-->
-                <a-select :default-value="item | getValue(index)" style="width: 200px" mode="multiple" @change="centerChange">
-                  <a-select-option :value="index + ':' + center.id + ':' + center.costCenterName" :itemIndex="index" v-for="center in selection.centers" :key="JSON.stringify(center)">
-                    {{ center.costCenterName }}
-                  </a-select-option>
-                </a-select>
-              </td>
-              <td>{{ item.itemType }}</td>
-              <td>{{ item.unitMaterial }}</td>
-              <td>{{ item.quantityMaterial }}</td>
-              <td>{{ item.unitPriceMaterial }}</td>
-              <td>{{ item.subAmountMaterial }}</td>
-              <td>{{ item.unitWork }}</td>
-              <td>{{ item.quantityWork }}</td>
-              <td>{{ item.unitPriceWork }}</td>
-              <td>{{ item.subAmountWork }}</td>
-              <td>{{ item.unitWorkMat }}</td>
-              <td>{{ item.quantityWorkMat }}</td>
-              <td>{{ item.unitPriceWorkMat }}</td>
-              <td>{{ item.subAmountWorkMat }}</td>
-              <td>{{ item.allAmount }}</td>
-            </tr>
-          </tbody>
-        </table>
+                </th>
+              </tr>
+              <tr>
+                <th rowspan="2">操作</th>
+                <th rowspan="2">
+                  带数项目
+                </th>
+                <th style="width: 60px" rowspan="2">清单编号</th>
+                <th style="width: 60px" rowspan="2">标段</th>
+                <th style="width: 5%" rowspan="2">楼栋</th>
+                <th style="width: 5%" rowspan="2">分部</th>
+                <th style="width: 5%" rowspan="2">分项</th>
+                <th style="width: 5%" rowspan="2">预留字段0</th>
+                <th style="width: 5%" rowspan="2">预留字段1</th>
+                <th style="width: 5%" rowspan="2">清单描述</th>
+                <th style="width: 5%" rowspan="2">业态成本中心</th>
+                <th style="width: 5%" rowspan="2">清单项类别</th>
+                <th style="width: 15%" colspan="4">供应</th>
+                <th style="width: 15%" colspan="4">安装</th>
+                <th style="width: 15%" colspan="4">供应+安装</th>
+                <th style="width: 5%" rowspan="2">合计</th>
+              </tr>
+              <tr>
+                <th>单位</th>
+                <th>工程量</th>
+                <th>单价</th>
+                <th>小计</th>
+                <th>单位</th>
+                <th>工程量</th>
+                <th>单价</th>
+                <th>小计</th>
+                <th>单位</th>
+                <th>工程量</th>
+                <th>单价</th>
+                <th>小计</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-if="!item.isDeleted" v-for="(item,index) in data.contractBQlst" :key="item.id">
+                <td>
+                  <div style="width: 220px;">
+                    <a-button @click="add(item.srNo)" :disabled="type === 'view'" icon="plus">
+                      添加子项
+                    </a-button>
+                    <a-button @click="del(index)" :disabled="type === 'view'" icon="close">
+                      删除
+                    </a-button>
+                  </div>
+                </td>
+                <td>
+                  <a-icon
+                    @click="item.isCarryData = !item.isCarryData"
+                    :type="item.isCarryData ? 'check-square' : 'border'"/>
+                </td>
+                <td>
+                  <a-input style="width:100px" :disabled="true" :value="item.srNo"></a-input>
+                </td>
+                <td>
+                  <a-input v-model="item.section"></a-input>
+                </td>
+                <td>
+                  <a-input v-model="item.building"></a-input>
+                </td>
+                <td></td>
+                <td></td>
+                <td>
+                  <a-input v-model="item.remark1"></a-input>
+                </td>
+                <td>
+                  <a-input v-model="item.remark2"></a-input>
+                </td>
+                <td>
+                  <a-input v-model="item.description"></a-input>
+                </td>
+                <td><!--{{ item.costCenter }}-->
+                  <a-select
+                    :default-value="item | getValue(index)"
+                    style="width: 200px"
+                    mode="multiple"
+                    @change="centerChange">
+                    <a-select-option
+                      :value="index + ':' + center.id + ':' + center.costCenterName"
+                      :itemIndex="index"
+                      v-for="center in selection.centers"
+                      :key="JSON.stringify(center)">
+                      {{ center.costCenterName }}
+                    </a-select-option>
+                  </a-select>
+                </td>
+                <td>
+                  <a-select
+                    placeholder="请选择"
+                    v-model="item.itemType"
+                    v-decorator="['item.itemType', { rules: [{required: true, message: '请选择'}] }]">
+                    <a-select-option v-for="(item, index) in selection.itemTypes" :key="index" :value="item.code">{{
+                      item.nameCN }}
+                    </a-select-option>
+                  </a-select>
+                </td>
+                <td>
+                  <a-select
+                    placeholder="请选择"
+                    v-model="item.unitMaterial"
+                    v-decorator="['item.unitMaterial', { rules: [{required: true, message: '请选择'}] }]">
+                    <a-select-option v-for="(item, index) in selection.unitTypes" :key="index" :value="item.nameCN">{{
+                      item.nameCN }}
+                    </a-select-option>
+                  </a-select>
+                </td>
+                <td>
+                  <a-input @change="valueChange(item)" v-model="item.quantityMaterial"></a-input>
+                </td>
+                <td>
+                  <a-input @change="valueChange(item)" v-model="item.unitPriceMaterial"></a-input>
+                </td>
+                <td>
+                  <a-input :disabled="true" v-model="item.subAmountMaterial"></a-input>
+                </td>
+                <td>
+                  <a-select
+                    placeholder="请选择"
+                    v-model="item.unitWork"
+                    v-decorator="['item.unitWork', { rules: [{required: true, message: '请选择'}] }]">
+                    <a-select-option v-for="(item, index) in selection.unitTypes" :key="index" :value="item.nameCN">{{
+                      item.nameCN }}
+                    </a-select-option>
+                  </a-select>
+                </td>
+                <td>
+                  <a-input @change="valueChange(item)" v-model="item.quantityWork"></a-input>
+                </td>
+                <td>
+                  <a-input @change="valueChange(item)" v-model="item.unitPriceWork"></a-input>
+                </td>
+                <td>
+                  <a-input :disabled="true" v-model="item.subAmountWork"></a-input>
+                </td>
+                <td>
+                  <a-select
+                    placeholder="请选择"
+                    v-model="item.unitWorkMat"
+                    v-decorator="['item.unitWorkMat', { rules: [{required: true, message: '请选择'}] }]">
+                    <a-select-option v-for="(item, index) in selection.unitTypes" :key="index" :value="item.nameCN">{{
+                      item.nameCN }}
+                    </a-select-option>
+                  </a-select>
+                </td>
+                <td>
+                  <a-input @change="valueChange(item)" v-model="item.quantityWorkMat"></a-input>
+                </td>
+                <td>
+                  <a-input @change="valueChange(item)" v-model="item.unitPriceWorkMat"></a-input>
+                </td>
+                <td>
+                  <a-input :disabled="true" v-model="item.subAmountWorkMat"></a-input>
+                </td>
+                <td>
+                  <a-input :disabled="true" v-model="item.allAmount"></a-input>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </a-col>
     </a-row>
   </a-form>
@@ -112,22 +190,43 @@
 
 <script>
   import { compare } from '@/utils/util'
+  import { Base as BaseService } from '@/api/base'
   import { SwaggerService } from '@/api/swagger.service'
   import { ContractService } from '@/views/contract/contract.service'
 
+  const contractTypes = {
+    '15': 'contract',
+    '16': 'sa',
+    '17': 'nsc'
+  }
+
   function getNo (str, key, items) {
+    console.log(str)
+    const isRoot = str === '0' || !str
+    let result
+    items = items.filter(item => !item.isDeleted)
+    items.sort((a, b) => {
+      return compare(b[key], a[key])
+    })
     if (items.length === 0) {
       return '1'
     }
-    const isRoot = str === '0' || !str
-    const filterItems = !isRoot ? items.filter(item => item[key].indexOf(str) === 0).sort((a, b) => b[key] - a[key]) : items
-    filterItems.sort((a, b) => {
-      return compare(b[key], a[key])
-    })
-    const arr = !isRoot ? filterItems[0][key].split('.').reverse() : filterItems[0][key].split('.')
-    arr[0] = parseInt(arr[0], 10) + 1 + ''
-    const result = !isRoot ? arr.reverse().join('.') : arr[0]
-    return isNaN(result) ? '1' : result
+    let arr
+    if (isRoot) {
+      arr = items[0][key].split('.')
+      arr[0] = parseInt(arr[0], 10) + 1 + ''
+      result = arr[0]
+    } else {
+      items = items.filter(item => item[key].indexOf(str) === 0)
+      arr = items[0][key].split('.')
+      if (items.length === 1) {
+        arr.push('0')
+      }
+      arr.reverse()
+      arr[0] = parseInt(arr[0], 10) + 1 + ''
+      result = arr.reverse().join('.')
+    }
+    return result
   }
 
   export default {
@@ -140,9 +239,17 @@
       }
     },
     created () {
+      BaseService.itemTypes(contractTypes[this.data.contract.contractCategory + 1]).then(res => {
+        console.log(res)
+        this.selection.itemTypes = res.result.data
+      })
       ContractService.centers(this.project.id).then(res => {
         console.log(res)
         this.selection.centers = res.result.data
+        this.$forceUpdate()
+      })
+      BaseService.unitTypes().then(res => {
+        this.selection.unitTypes = res.result.data
         this.$forceUpdate()
       })
     },
@@ -151,15 +258,13 @@
         return items.filter(item => !item.isDeleted)
       },
       getValue (item, index) {
-        console.log(item)
         const values = []
-        const ids = item.costCenter.split(':')
-        const names = item.costCenterName.split(':')
+        const ids = item.costCenter ? item.costCenter.split(':') : []
+        const names = item.costCenterName ? item.costCenterName.split(':') : []
         ids.forEach((id, idsIndex) => {
           const value = index + ':' + id + ':' + names[idsIndex]
           values.push(value)
         })
-        console.log(values)
         return values
       }
     },
@@ -185,16 +290,28 @@
       add (stringNo) {
         const newSrNo = getNo(stringNo, 'srNo', this.data.contractBQlst)
         const data = SwaggerService.getForm('ContractBQInputDto')
-        data.contractID = this.data.contract.id
+        data.isTemp = true
+        data.contractID = this.id
         data.srNo = newSrNo
+        data.isCarryData = false
         this.data.contractBQlst.push(data)
       },
       del (index) {
-        this.data.contractBQlst[index].isDeleted = true
+        if (this.data.contractBQlst[index].isTemp) {
+          this.data.contractBQlst.splice(index, 1)
+        } else {
+          this.data.contractBQlst[index].isDeleted = true
+        }
+        this.$forceUpdate()
       },
       clear () {
+        const list = []
         this.data.contractBQlst.forEach(item => {
           item.isDeleted = true
+          if (!item.isTemp) {
+            list.push(item)
+          }
+          this.$forceUpdate()
         })
       },
       centerChange (values) {
@@ -219,6 +336,12 @@
         })
         item.costCenter = ids
         item.costCenterName = names
+      },
+      valueChange (item) {
+        item.subAmountMaterial = item.quantityMaterial * item.unitPriceMaterial
+        item.subAmountWork = item.quantityWork * item.unitPriceWork
+        item.subAmountWorkMat = item.quantityWorkMat * item.unitPriceWorkMat
+        item.allAmount = item.subAmountMaterial + item.subAmountWork + item.subAmountWorkMat
       }
     }
   }
@@ -263,6 +386,10 @@
           border-width: 0 0 1px 1px;
           border-style: solid;
           border-color: #ccc;
+
+          .ant-input {
+            min-width: 60px;
+          }
 
           button {
             margin-right: 10px;
