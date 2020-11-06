b0b358a691161a8e35c726437a189cff63404459
wzh66 <wzh514500>
2020-11-06 18:05:36 +0800

    更新付款管理页面

--- src/views/pay/unsigned/components/AttachmentList.vue
+++ src/views/pay/unsigned/components/AttachmentList.vue
@@ -1,52 +1,168 @@
 <template>
   <div>
     <a-col :md="24" :sm="24">
-      <table>
-        <thead>
-        <tr>
-          <th colspan="5">
-            <a-button icon="plus" @click="add('detailList')">
-              新增
-            </a-button>
-          </th>
-        </tr>
-        <tr>
-          <th>操作</th>
-          <th>文件名称</th>
-          <th>文件数量</th>
-          <th>上传时间</th>
-          <th>上传人</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td>
-            <a-button @click="del(index)" :disabled="type === 'view'" icon="close">
-              删除
-            </a-button>
-          </td>
-          <td>
-
-          </td>
-          <td>
-
-          </td>
-          <td>
-
-          </td>
-          <td>
-
-          </td>
-        </tr>
-        </tbody>
-      </table>
+      <div class="table-wrapper">
+        <table>
+          <thead>
+          <tr>
+            <th colspan="5">
+              <a-button icon="plus" @click="add" :disabled="type === 'view'">
+                新增
+              </a-button>
+            </th>
+          </tr>
+          <tr>
+            <th>操作</th>
+            <th>文件名称</th>
+            <th>文件数量</th>
+            <th>上传时间</th>
+            <th>上传人</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="(item,index) in fileList" :key="index">
+            <td>
+              <a-upload name="file"
+                        :disabled="type === 'view'"
+                        :multiple="false"
+                        :before-upload="beforeUpload">
+                <a-button @click="choose(index)">请选择</a-button>
+              </a-upload>
+              <a-button @click="del(index)" :disabled="type === 'view'" icon="close">
+                删除
+              </a-button>
+            </td>
+            <td>
+              <a :href="item.fileUrl" target="_blank" v-if="item.fileName">{{item.fileName}}</a>
+            </td>
+            <td><span v-if="item.fileName">1</span></td>
+            <td>
+              <a-input v-model="item.creationTime" :disabled="true"></a-input>
+            </td>
+            <td>
+              <a-input v-model="item.creatorUser" :disabled="true"></a-input>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
     </a-col>
   </div>
 </template>
 
 <script>
+    import { Base as BaseService } from '@/api/base'
+
     export default {
-        name: 'AttachmentList'
+        name: 'AttachmentList',
+        data () {
+            return {
+                fileList: [],
+                index: 0
+            }
+        },
+        props: {
+            data: {
+                type: Object,
+                default: null
+            },
+            type: {
+                type: String,
+                default: 'view'
+            },
+            id: {
+                type: String,
+                default: '0'
+            },
+            masterID: {
+                type: Number,
+                default: null
+            }
+        },
+        created () {
+            if (this.type !== 'create') {
+                BaseService.masterID(this.id).then(_res => {
+                    this.getFileList(_res.result.data)
+                })
+            }
+        },
+        methods: {
+            getFileList (id) {
+                BaseService.fileList(id, this.id, 'paymentOther', '').then(res => {
+                    this.fileList = res.result.data
+                })
+            },
+            add () {
+                const item = {
+                    isDeleted: false,
+                    isTemp: true,
+                    fileName: '',
+                    fileUrl: '',
+                    fileId: '',
+                    creationTime: '',
+                    creatorUser: ''
+                }
+                if (this.fileList) {
+                    this.fileList.push(item)
+                } else {
+                    let fileList = []
+                    fileList.push(item)
+                    this.fileList = fileList
+                }
+
+                this.$forceUpdate()
+            },
+            choose (index) {
+                this.index = index
+            },
+            del (index) {
+                if (this.fileList[index].isTemp) {
+                    if (this.fileList[index].fileId) {
+                        this.removeFile(this.fileList[index].fileId)
+                    }
+                    this.fileList.splice(index, 1)
+                } else {
+                    this.removeFile(this.fileList[index].fileId)
+                    this.fileList[index].isDeleted = true
+                }
+                this.$forceUpdate()
+            },
+            removeFile (id) {
+                BaseService.removeFile(id).then(res => {
+                    console.log(res)
+                })
+            },
+            beforeUpload (file) {
+                this.handleUpload(file)
+                return false
+            },
+            handleUpload (file) {
+                const formData = new FormData()
+                formData.append('file', file)
+                formData.append('masterId', this.masterID)
+                formData.append('businessID', this.type === 'create' ? '' : this.id)
+                formData.append('businessType', 'paymentOther')
+                formData.append('subInfo1', file.name) // 文件名
+                this.uploading = true
+                const _this = this
+                this.$http.post('/api/services/app/UploadAppservice/CommonUpload', formData, {
+                    contentType: false,
+                    processData: false,
+                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+                })
+                    .then((response) => {
+                        this.$emit('on-change-masterId', response.result.data.masterID)
+                        this.fileList[this.index].fileName = response.result.data.fileName
+                        this.fileList[this.index].fileId = response.result.data.id
+                        this.fileList[this.index].creationTime = response.result.data.creationTime
+                        this.fileList[this.index].creatorUser = response.result.data.creatorUser
+                        this.fileList[this.index].fileUrl = response.result.data.fileUrl
+                        _this.$message.success('上传成功')
+                        _this.$emit('ok', response.url)
+                        _this.visible = false
+                    })
+            },
+        }
     }
 </script>
 
--- src/views/pay/unsigned/unsigned.service.js
+++ src/views/pay/unsigned/unsigned.service.js
@@ -1,8 +1,16 @@
 import request from '@/utils/request'
+import { SignedService } from '@/views/pay/signed/signed.service'
 
 const API = {
   items: '/api/services/app/PaymentOther/GetPaymentOtherPage',
   item: '/api/services/app/PaymentOther/GetPaymentOtherInfo',
+  payTypeList: '/api/services/app/PaymentOther/GetPaymentBusinessTypeList',
+  expenseAccountTypeList: '/api/services/app/PaymentOther/GetExpenseAccountTypeList',
+  moneyTypes: '/api/services/app/PaymentOther/GetPaymenTypeList',
+  billTypes: '/api/services/app/PaymentOther/GetPaymentBillTypeList',
+  vendorTypes: '/api/services/app/PaymentOther/GetPaymentOtherVendor',
+  create: '/api/services/app/PaymentOther/CreatePaymentOther',
+  update: '/api/services/app/PaymentOther/UpdatePaymentOther',
 }
 
 const UnSignedService = {}
@@ -27,6 +35,62 @@ UnSignedService.item = function (gid) {
     url: API.item,
     method: 'get',
     params: { gid }
+  })
+}
+
+UnSignedService.payTypeList = function () {
+  return request({
+    url: API.payTypeList,
+    method: 'get',
+    params: {}
+  })
+}
+
+UnSignedService.expenseAccountTypeList = function () {
+  return request({
+    url: API.expenseAccountTypeList,
+    method: 'get',
+    params: {}
+  })
+}
+
+UnSignedService.moneyTypes = function () {
+  return request({
+    url: API.moneyTypes,
+    method: 'get',
+    params: {}
+  })
+}
+
+UnSignedService.billTypes = function () {
+  return request({
+    url: API.billTypes,
+    method: 'get',
+    params: {}
+  })
+}
+
+UnSignedService.vendorTypes = function (vendorCodeOrName) {
+  return request({
+    url: API.vendorTypes,
+    method: 'get',
+    params: { vendorCodeOrName }
+  })
+}
+
+UnSignedService.create = function (parameter) {
+  return request({
+    url: API.create,
+    method: 'POST',
+    data: parameter
+  })
+}
+
+UnSignedService.update = function (parameter) {
+  return request({
+    url: API.update,
+    method: 'POST',
+    data: parameter
   })
 }
 
