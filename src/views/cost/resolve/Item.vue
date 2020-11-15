15ca5ada44da4af0c8747cd2e1d8e32fb8b730f9
pengcl <3559996@qq.com>
2020-11-15 12:46:04 +0800

    完善供应商，合同，项目，业态成本中心等

--- src/views/cost/resolve/Item.vue
+++ src/views/cost/resolve/Item.vue
@@ -28,18 +28,18 @@
             :alert="false"
             showPagination="auto"
           >
-        <span slot="cost" slot-scope="text">
-          <p style="text-align: center">
-            <span style="font-weight: bold;padding-right: 10px">{{text}}</span>
-          </p>
-        </span>
+            <span slot="cost" slot-scope="text">
+              <p style="text-align: center">
+                <span style="font-weight: bold;padding-right: 10px">{{ text }}</span>
+              </p>
+            </span>
             <span slot="action" slot-scope="text, record">
-          <template>
-            <a-button v-if="record.isCreate" @click="showModal(record,'add')" type="success" icon="form" title="新增">
-            </a-button>
-            {{record.BudgetTitle}}
-          </template>
-        </span>
+              <template>
+                <a-button v-if="record.isCreate" @click="showModal(record,'add')" type="success" icon="form" title="新增">
+                </a-button>
+                {{ record.elementInfoNameCN }}
+              </template>
+            </span>
           </s-table>
         </a-col>
       </a-row>
@@ -54,271 +54,321 @@
 </template>
 
 <script>
-  import {CostService} from "@/views/cost/cost.service";
-  import {Ellipsis, STable} from "@/components";
-  import ResolveModal from '@/views/cost/resolve/modal/ResolveModal'
-  import StepByStepModal from "@/views/list/modules/StepByStepModal";
+import { CostService } from '@/views/cost/cost.service'
+import { Ellipsis, STable } from '@/components'
+import ResolveModal from '@/views/cost/resolve/modal/ResolveModal'
+import StepByStepModal from '@/views/list/modules/StepByStepModal'
 
-  function fixedList (res, params) {
-    const result = {}
-    result.pageSize = params.pageSize
-    result.pageNo = params.pageNo
-    if (res.result.data) {
-      result.totalPage = Math.ceil(res.result.data.length / params.pageSize)
-      result.totalCount = res.result.data.length
-      result.data = formatList(res.result.data)
-    } else {
-      result.totalPage = 0
-      result.totalCount = 0
-      result.data = []
+function fixedList (res, params) {
+  const result = {}
+  result.pageSize = params.pageSize
+  result.pageNo = params.pageNo
+  if (res.result.data) {
+    result.totalPage = Math.ceil(res.result.data.length / params.pageSize)
+    result.totalCount = res.result.data.length
+    result.data = formatList(res.result.data)
+  } else {
+    result.totalPage = 0
+    result.totalCount = 0
+    result.data = []
+  }
+  return result
+}
+
+function formatList (items) {
+  const list = []
+  items.forEach(item => {
+    if (item.childs) {
+      item.children = formatList(item.childs)
     }
-    return result
+    list.push(item)
+  })
+  return list
+}
+
+function getBudgetItems (elementInfoId, list, item) {
+  if (item.tradeBudgetItems && item.tradeBudgetItems.length > 0) {
+    if (item.elementInfoId === elementInfoId) {
+      list = item.tradeBudgetItems
+    }
   }
+  return list
+}
 
-  function formatList (items) {
-    const list = []
-    items.forEach(item => {
-      if (item.childs) {
-        item.children = formatList(item.childs)
-      }
-      list.push(item)
-    })
-    return list
-  }
-
-  function getList(items,costCenters){
-    const list = []
-    items.forEach(item => {
-      const obj = {}
-      obj.elementInfoId = item.elementInfoId
-      obj.elementInfoCode = item.elementInfoCode
-      obj.BudgetTitle = item.elementInfoNameCN
-
-      costCenters.forEach(center => {
-        const costName = 'cost' + center.costCenterId
-        obj[costName] = item.amount
+function getBudgetList (elementInfoId, items) {
+  let obj = null
+  items.forEach(item => {
+    if (item.childs && item.childs.length > 0) {
+      const temp = getBudgetList(elementInfoId, item.childs)
+      if (temp) { obj = temp }
+    } else if (item.elementItem && item.elementItem.childs) {
+      const temp = getBudgetList(elementInfoId, item.elementItem.childs)
+      if (temp) { obj = temp }
+    } else if (item.tradeBudgetItems && item.tradeBudgetItems.length > 0) {
+      item.tradeBudgetItems.forEach((budget, index) => {
+        if (budget.elementInfoId === elementInfoId) {
+            obj = {}
+            obj['name'] = 'cost' + budget.costCenterId
+            obj['value'] = budget.budgetValue
+        }
       })
-      if (item.childs && item.childs.length>0) {
-        const childrenList = getList(item.childs,costCenters)
-        obj.childs = childrenList
-        obj.children = obj.childs
-      }
-      list.push(obj)
-      //插入General Trade行
-      if( item.tradeBudgetInfo && item.childs && item.childs.length==0){
-        const gt = {}
-        costCenters.forEach((center,index) => {
-          gt.elementInfoId = item.elementInfoId+new Date().getTime()
-          gt.TradeTypeNameCN = 'General Trade'
-          gt.BudgetTitle = ''
-          const costName = 'cost' + center.costCenterId
-          gt[costName] = item.tradeBudgetInfo.budgetValue
-        })
-        list.push(gt)
-        obj.costCenters = costCenters
-        obj.isCreate = true
-      }else{
-        obj.costCenters = null
-        obj.isCreate = false
-      }
+    }
+  })
+  return obj
+}
+
+function getList (items, costCenters) {
+  const list = []
+  items.forEach(item => {
+    // 插入科目
+    const obj = {}
+    obj.elementInfoId = item.elementInfoId
+    obj.elementInfoCode = item.elementInfoCode
+    obj.elementInfoNameCN = item.elementInfoNameCN
+
+    costCenters.forEach(center => {
+      const costName = 'cost' + center.costCenterId
+      obj[costName] = item.amount
     })
-    return list
-  }
-
-  const defaultColumns = [
-    {
-      title: '行业名称',
-      width: '350px',
-      dataIndex: 'BudgetTitle',
-      scopedSlots: { customRender: 'action' }
-    },
-    {
-      title: '行业名称',
-      width: '300px',
-      dataIndex: 'TradeTypeNameCN'
+    if (item.childs && item.childs.length > 0) {
+      const childrenList = getList(item.childs, costCenters)
+      obj.childs = childrenList
+      obj.children = obj.childs
     }
-  ]
+    list.push(obj)
+    // 插入行业预算
+    if (item.tradeBudgetItems && item.tradeBudgetItems.length > 0) {
+      item.tradeBudgetItems.forEach((budget, index) => {
+        const budgetItem = {}
+        budgetItem.elementInfoId = budget.elementInfoId + new Date().getTime()
+        budgetItem.BudgetTitle = budget.budgetTitle
+        budgetItem.elementInfoNameCN = budget.elementInfoNameCN
+        costCenters.forEach(center => {
+          console.log('center'+center.costCenterId,center)
+          const result = getBudgetList(budget.elementInfoId, center.elementItem.childs)
+          console.log(result)
 
-  const columns = defaultColumns
+          if (result !== null) {
+            budgetItem[result['name']] = result['value']
+          }
+        })
+        list.push(budgetItem)
+      })
+    }
 
-  export default {
-    name: 'list',
-    components: {
-      STable,
-      Ellipsis,
-      ResolveModal
-    },
-    data () {
-      this.columns = columns
+    // 插入General Trade行
+    if (item.tradeBudgetInfo && item.childs && item.childs.length == 0) {
+      const gt = {}
+      costCenters.forEach((center, index) => {
+        gt.elementInfoId = item.elementInfoId + new Date().getTime()
+        gt.BudgetTitle = 'General Trade'
+        gt.elementInfoNameCN = ''
+        const costName = 'cost' + center.costCenterId
+        gt[costName] = item.tradeBudgetInfo.budgetValue
+      })
+      list.push(gt)
+      obj.costCenters = costCenters
+      obj.isCreate = true
+    } else {
+      obj.costCenters = null
+      obj.isCreate = false
+    }
+  })
+  return list
+}
+
+const defaultColumns = [
+  {
+    title: '行业名称',
+    width: '350px',
+    dataIndex: 'elementInfoNameCN',
+    scopedSlots: { customRender: 'action' }
+  },
+  {
+    title: '行业名称',
+    width: '300px',
+    dataIndex: 'BudgetTitle'
+  }
+]
+
+const columns = defaultColumns
+
+export default {
+  name: 'List',
+  components: {
+    STable,
+    Ellipsis,
+    ResolveModal
+  },
+  data () {
+    this.columns = columns
+    return {
+      auditStatus: '',
+      cities: [],
+      visible: false,
+      confirmLoading: false,
+      mdl: null,
+      // 高级搜索 展开/关闭
+      advanced: false,
+      // 查询参数
+      queryParam: { ProjectGUID: this.$route.query.ProjectGUID },
+      // 加载数据方法 必须为 Promise 对象
+      loadData: parameter => {
+        const _columns = JSON.parse(JSON.stringify(defaultColumns))
+        const requestParameters = Object.assign({}, parameter, this.queryParam, { MaxResultCount: 1000, ProjectGUID: this.queryParam.ProjectGUID, ElementTypeId: this.id })
+        const result = {
+          result: {
+            data: []
+          }
+        }
+        return CostService.resolveTreeItems(requestParameters).then(res => {
+          if (res.result.data) {
+            res.result.data.forEach(item => {
+              // 组装动态列
+              _columns.push(
+                {
+                  title: item.costCenterName,
+                  children: [
+                    {
+                      title: 'Budget Value',
+                      dataIndex: 'cost' + item.costCenterId,
+                      scopedSlots: { customRender: 'cost' }
+                    }
+                  ]
+                }
+              )
+            })
+            // 组装动态列对应的行数据
+            const list = res.result.data[0].elementItem.childs
+            result.result.data = getList(list, res.result.data)
+            this.columns = _columns
+            this.$forceUpdate()
+          }
+          return fixedList(result, parameter)
+        })
+      }
+    }
+  },
+  created () {
+
+  },
+  computed: {
+    rowSelection () {
       return {
-        auditStatus: '',
-        cities: [],
-        visible: false,
-        confirmLoading: false,
-        mdl: null,
-        // 高级搜索 展开/关闭
-        advanced: false,
-        // 查询参数
-        queryParam: { ProjectGUID:this.$route.query.ProjectGUID },
-        // 加载数据方法 必须为 Promise 对象
-        loadData: parameter => {
-          const _columns = JSON.parse(JSON.stringify(defaultColumns))
-          const requestParameters = Object.assign({}, parameter, this.queryParam, { MaxResultCount : 1000 ,ProjectGUID: this.queryParam.ProjectGUID,ElementTypeId: this.id})
-          const result = {
-            result: {
-              data: []
-            }
-          }
-          return CostService.resolveTreeItems(requestParameters).then(res => {
-            console.log(res.result.data)
-            if(res.result.data){
-              res.result.data.forEach(item => {
-                //组装动态列
-                _columns.push(
-                  {
-                    title: item.costCenterName,
-                    children: [
-                      {
-                        title: 'Budget Value',
-                        dataIndex: 'cost' + item.costCenterId,
-                        scopedSlots: { customRender: 'cost' }
-                      }
-                    ]
-                  }
-                )
-              })
-              //组装动态列对应的行数据
-              const list = res.result.data[0].elementItem.childs
-              result.result.data = getList(list,res.result.data)
-              this.columns = _columns
-              this.$forceUpdate()
-            }
-            console.log(result)
-            return fixedList(result, parameter)
-          })
-        }
+        selectedRowKeys: this.selectedRowKeys,
+        onChange: this.onSelectChange
       }
     },
-    created () {
-
+    id () {
+      return this.$route.params.id
     },
-    computed: {
-      rowSelection () {
-        return {
-          selectedRowKeys: this.selectedRowKeys,
-          onChange: this.onSelectChange
-        }
-      },
-      id () {
-        return this.$route.params.id
-      },
-      type () {
-        return this.$route.query.type
-      },
-      ProjectGUID () {
-        return this.$route.query.ProjectGUID
+    type () {
+      return this.$route.query.type
+    },
+    ProjectGUID () {
+      return this.$route.query.ProjectGUID
+    }
+  },
+  methods: {
+    handleToItem (record) {
+      this.$router.push({ path: `/cost/enact/item/${record.id}?type=view&ProjectGUID=${this.queryParam.ProjectGUID}` })
+    },
+    handleToEdit (record) {
+      this.$router.push({ path: `/cost/enact/item/${record.id}?type=edit&ProjectGUID=${this.queryParam.ProjectGUID}` })
+    },
+    handleToAdd () {
+      this.$router.push({ path: `/cost/enact/item/0?type=add` })
+    },
+    onChange (value, option) {
+      if (value.length >= 2) {
+        this.queryParam.ProjectGUID = value
+        this.$refs.table.refresh(true)
+      } else {
+        this.queryParam.ProjectGUID = ''
+        this.$refs.table.refresh(true)
       }
     },
-    methods: {
-      handleToItem (record) {
-        this.$router.push({ path: `/cost/enact/item/${record.id}?type=view&ProjectGUID=${this.queryParam.ProjectGUID}` })
-      },
-      handleToEdit (record) {
-        this.$router.push({ path: `/cost/enact/item/${record.id}?type=edit&ProjectGUID=${this.queryParam.ProjectGUID}` })
-      },
-      handleToAdd () {
-        this.$router.push({ path: `/cost/enact/item/0?type=add` })
-      },
-      onChange (value,option) {
-        if (value.length >= 2) {
-          this.queryParam.ProjectGUID = value
-          this.$refs.table.refresh(true)
-        } else {
-          this.queryParam.ProjectGUID = ''
-          this.$refs.table.refresh(true)
+    onSelect (value, option) {
+      this.auditStatus = option.dataRef.auditStatus
+    },
+    back () {
+      this.$router.push({ path: `/cost/enact/list` })
+    },
+    showModal (record, type) {
+      CostService.elementTradeTypes({ Id: record.elementInfoId }).then(res => {
+        record.elementTradeTypes = JSON.parse(JSON.stringify(res.result.data))
+        record.ProjectGUID = this.ProjectGUID
+        record.type = type
+        this.$refs.resolveModal.show(record)
+      })
+    },
+    // handleOk(e) {
+    //   this.ModalText = 'The modal will be closed after two seconds';
+    //   this.confirmLoading = true;
+    //   setTimeout(() => {
+    //     this.visible = false;
+    //     this.confirmLoading = false;
+    //   }, 2000);
+    // },
+    // handleCancel(e) {
+    //   console.log('Clicked cancel button');
+    //   this.visible = false;
+    // },
+    createGT () {
+      CostService.createGT({ projectGUID: this.ProjectGUID, planPackageGUID: this.ProjectGUID }).then(res => {
+        if (res.result.statusCode === 200) {
+          this.$message.info('GeneralTrade已触发生成')
         }
-      },
-      onSelect (value,option) {
-        this.auditStatus = option.dataRef.auditStatus
-      },
-      back () {
-        this.$router.push({ path: `/cost/enact/list?ProjectGUID=${this.ProjectGUID}` })
-      },
-      showModal(record,type) {
-        CostService.elementTradeTypes({Id:record.elementInfoId}).then(res => {
-          record.elementTradeTypes = JSON.parse(JSON.stringify(res.result.data))
-          record.ProjectGUID = this.ProjectGUID
-          record.type = type
-          this.$refs.resolveModal.show(record)
-        })
-      },
-      handleOk(e) {
-        this.ModalText = 'The modal will be closed after two seconds';
-        this.confirmLoading = true;
-        setTimeout(() => {
-          this.visible = false;
-          this.confirmLoading = false;
-        }, 2000);
-      },
-      handleCancel(e) {
-        console.log('Clicked cancel button');
-        this.visible = false;
-      },
-      createGT() {
-        CostService.createGT({projectGUID: this.ProjectGUID, planPackageGUID : this.ProjectGUID}).then(res => {
-          if (res.result.statusCode === 200) {
-            this.$message.info('GeneralTrade已触发生成')
-          }
-        })
-      },
+      })
     }
   }
+}
 </script>
 
 <style  lang="less" scoped>
-  table {
-    margin: 15px 0;
-    width: 100%;
-    border-width: 1px 1px 0 0;
-    border-radius: 3px 3px 0 0;
-    border-style: solid;
-    border-color: #ccc;
+table {
+  margin: 15px 0;
+  width: 100%;
+  border-width: 1px 1px 0 0;
+  border-radius: 3px 3px 0 0;
+  border-style: solid;
+  border-color: #ccc;
 
-    thead {
-      tr {
-        &:first-child{
-          th{
-            background-color: #f5f5f5;
-          }
-        }
-        th {
-          background-color: #06c;
-          color: #fff;
-          font-weight: normal;
-          border-width: 0 0 1px 1px;
-          border-style: solid;
-          border-color: #ccc;
-
-          button {
-            margin-right: 10px;
-          }
+  thead {
+    tr {
+      &:first-child{
+        th{
+          background-color: #f5f5f5;
         }
       }
-    }
+      th {
+        background-color: #06c;
+        color: #fff;
+        font-weight: normal;
+        border-width: 0 0 1px 1px;
+        border-style: solid;
+        border-color: #ccc;
 
-    tbody {
-      tr {
-        td {
-          padding: 0.5em 0.6em 0.4em 0.6em !important;
-          border-width: 0 0 1px 1px;
-          border-style: solid;
-          border-color: #ccc;
-
-          button {
-            margin-right: 10px;
-          }
+        button {
+          margin-right: 10px;
         }
       }
     }
   }
+
+  tbody {
+    tr {
+      td {
+        padding: 0.5em 0.6em 0.4em 0.6em !important;
+        border-width: 0 0 1px 1px;
+        border-style: solid;
+        border-color: #ccc;
+
+        button {
+          margin-right: 10px;
+        }
+      }
+    }
+  }
+}
 </style>
