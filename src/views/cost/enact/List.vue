c365c2776e2a95f09c9f652340c0b68e6f103793
HHC0541 <85995442@qq.com>
2021-01-15 10:38:44 +0800

    调整按钮判断

--- src/views/cost/enact/List.vue
+++ src/views/cost/enact/List.vue
@@ -78,7 +78,7 @@
           <template>
             {{ record.code }}
             <a-button
-              :disabled="auditStatus !== '已审核'"
+              :disabled="auditStatus === '审核中'"
               v-if="ac('VIEW')"
               @click="handleToItem(record)"
               type="success"
@@ -86,7 +86,7 @@
               title="查看">
             </a-button>
             <a-button
-              :disabled="auditStatus !== '已审核'"
+              :disabled="auditStatus === '审核中'"
               v-if="ac('EDIT') && auditStatus !== '审核中'"
               @click="handleToEdit(record)"
               type="primary"
