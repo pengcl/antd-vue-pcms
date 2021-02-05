dd769e0b826727c14f4b6ad6946e47426c2ceddc
wzh66 <wzh514500>
2021-02-05 15:42:44 +0800

    更新合同分摊工具

--- src/views/contract/components/contractList/ShareToolModal.vue
+++ src/views/contract/components/contractList/ShareToolModal.vue
@@ -4,36 +4,88 @@
     width="90%"
     :maskClosable="false"
     title="分摊工具"
-    @cancel="handleCancel"
     :confirm-loading="confirmLoading"
     @ok="handleOk">
-    <a-row>
-      <a-col>
-        <div
-          style="padding-top:5px; padding-bottom:5px;padding-left:30px;background-color:#f5f5f5;border-bottom:0;border:1px solid #ccc;"
-        >
-          <a-button icon="plus" @click="add()"> 新增</a-button>
-        </div>
-        <a-form-model ref="form" :model="data" :rules="tableRules" :label-col="{ span: 8 }" :wrapper-col="{ span: 16 }">
-          <a-table
-            :row-key="record => record.id"
-            :columns="columns"
-            :data-source="data.tableData"
-            bordered
-            ref="table"
-            :pagination="false"
+    <template slot="footer">
+      <a-button
+        key="compute"
+        @click="computeShareAmount"
+        type="success"
+        :disabled="selectedRows.length < 1">
+        分摊金额计算
+      </a-button>
+      <a-button key="back" @click="handleCancel">
+        关闭
+      </a-button>
+      <a-button
+        key="submit"
+        type="primary"
+        @click="handleOk">
+        确认
+      </a-button>
+    </template>
+    <a-form-model ref="_form" :model="data" :rules="rules" :label-col="{ span: 8 }" :wrapper-col="{ span: 16 }">
+      <a-row :gutter="48">
+        <a-col :md="8" :sm="24">
+          <a-form-model-item
+            label="清单项类别"
+            prop="itemType"
+            :rules="[{ required: true, message: '请选择清单项类别'}]">
+            <a-select placeholder="请选择" v-model="data.itemType">
+              <a-select-option
+                v-for="(item, index) in selection.itemTypes"
+                :key="index"
+                :value="item.code"
+              >{{ item.nameCN }}
+              </a-select-option>
+            </a-select>
+          </a-form-model-item>
+        </a-col>
+        <a-col :md="8" :sm="24">
+          <a-form-model-item
+            label="分摊方式"
+            prop="shareType"
+            :rules="[{ required: true, message: '请选择分摊方式'}]"
           >
-            <span slot="action" slot-scope="text, item, index">
-                <template>
-                <a-button @click="del(index)" icon="close"> 删除 </a-button>
-                </template>
-            </span>
+            <a-select placeholder="请选择" v-model="data.shareType">
+              <a-select-option
+                v-for="(item, index) in selection.shareTypes"
+                :key="index"
+                :value="item.id"
+              >{{ item.nameCN }}
+              </a-select-option>
+            </a-select>
+          </a-form-model-item>
+        </a-col>
+        <a-col :md="8" :sm="24">
+          <a-form-model-item
+            label="总金额"
+            prop="allAmount"
+            :rules="[{ required: true, message: '请输入总金额'}]"
+          >
+            <a-input-number
+              v-model="data.allAmount"
+              :formatter="value => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
+              :parser="value => value.replace(/\元\s?|(,*)/g, '')"
+              :precision="2"
+            ></a-input-number>
+          </a-form-model-item>
+        </a-col>
+      </a-row>
+    </a-form-model>
+    <a-form-model ref="form" :model="data" :rules="tableRules" :label-col="{ span: 8 }" :wrapper-col="{ span: 16 }">
+      <a-table
+        style="margin-top: 10px"
+        :row-key="record => record.costCenter"
+        :columns="columns"
+        :data-source="data.tableData"
+        bordered
+        ref="table"
+        :pagination="false"
+        :rowSelection="rowSelection"
+      >
             <span slot="costCenter" slot-scope="text, item, index">
-                <a-form-model-item
-                  :prop="'tableData.' + index + '.costCenterIdlst'"
-                  :rules="[{ required: true, message: '请选择成本中心' }]"
-                >
-                <a-select v-model="item.costCenterIdlst" mode="multiple" style="width:270px;">
+                <a-select v-model="item.costCenter" style="width:270px;" :disabled="true">
                     <a-select-option
                       :value="center.id + ''"
                       v-for="center in selection.centers"
@@ -43,56 +95,52 @@
                     {{ center.costCenterName }}
                     </a-select-option>
                 </a-select>
-                </a-form-model-item>
             </span>
-            <span slot="itemType" slot-scope="text, item, index">
-                <a-form-model-item
-                  :prop="'tableData.' + index + '.itemType'"
-                  :rules="[{ required: true, message: '请选择清单项类别' }]"
-                >
-                <a-select placeholder="请选择" v-model="item.itemType" style="width: 200px">
-                    <a-select-option
-                      v-for="(item, index) in selection.itemTypes"
-                      :key="index"
-                      :value="item.code"
-                    >{{ item.nameCN }}
+
+        <span slot="secCostAllocateTypeID" slot-scope="text, item, index">
+                <a-select v-model="item.secCostAllocateTypeID" style="width: 200px" :disabled="true">
+                    <a-select-option v-for="(item,index) in secCostAllocateTypes"
+                                     :key="index"
+                                     :value="item.id">{{item.nameCN}}
                     </a-select-option>
                 </a-select>
-                </a-form-model-item>
-            </span>
-            <span slot="shareType" slot-scope="text, item, index">
-                <a-form-model-item
-                  :prop="'tableData.' + index + '.shareType'"
-                  :rules="[{ required: true, message: '请选择分摊方式' }]"
-                >
-                <a-select placeholder="请选择" v-model="item.shareType" style="width: 200px">
-                    <a-select-option
-                      v-for="(item, index) in selection.shareTypes"
-                      :key="index"
-                      :value="item.id"
-                    >{{ item.nameCN }}
-                    </a-select-option>
-                </a-select>
-                </a-form-model-item>
-            </span>
-            <div slot="allAmount" slot-scope="text, item, index">
-              <a-form-model-item
-                :prop="'tableData.' + index + '.allAmount'"
-                :rules="[{ required: true, message: '请输入金额' }]"
-              >
-                <a-input-number
-                  style="min-width:200px;margin-top: 10px"
-                  v-model="item.allAmount"
-                  :formatter="value => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
-                  :parser="value => value.replace(/\元\s?|(,*)/g, '')"
-                  :precision="2"
-                ></a-input-number>
-              </a-form-model-item>
-            </div>
-          </a-table>
-        </a-form-model>
-      </a-col>
-    </a-row>
+         </span>
+
+        <span slot="totalCFAExcludeParking" slot-scope="text">
+                {{text | NumberFormat}}
+        </span>
+
+        <span slot="totalGFA" slot-scope="text">
+                {{text | NumberFormat}}
+        </span>
+
+        <span slot="rate" slot-scope="text, item, index">
+          <a-form-model-item
+            :prop="'tableData.' + index + '.rate'"
+          >
+            <a-input-number
+              style="min-width:200px;margin-top: 12px"
+              v-model="item.rate"
+              :min="0"
+              :max="100"
+              :formatter="value => `${value}%`"
+              :parser="value => value.replace('%', '')"
+            ></a-input-number>
+          </a-form-model-item>
+        </span>
+
+        <div slot="allAmount" slot-scope="text, item, index">
+          <a-input-number
+            :disabled="true"
+            style="min-width:200px;"
+            v-model="item.allAmount"
+            :formatter="value => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
+            :parser="value => value.replace(/\元\s?|(,*)/g, '')"
+            :precision="2"
+          ></a-input-number>
+        </div>
+      </a-table>
+    </a-form-model>
   </a-modal>
 </template>
 
@@ -100,38 +148,39 @@
     import { STable, Ellipsis } from '@/components'
     import { Base as BaseService } from '@/api/base'
     import { ContractService } from '@/views/contract/contract.service'
+    import { CostService } from '@/views/project/cost/cost.service'
 
     const columns = [
         {
-            title: '操作',
-            dataIndex: 'action',
-            scopedSlots: { customRender: 'action' },
-            width: '100px'
-        },
-        {
             title: '业态成本中心',
-            dataIndex: 'costCenterIdlst',
+            dataIndex: 'costCenter',
             scopedSlots: { customRender: 'costCenter' },
-            width: '270px'
         },
         {
-            title: '清单项类别',
-            dataIndex: 'itemType',
-            scopedSlots: { customRender: 'itemType' },
-            width: '220px'
+            title: '二次分摊',
+            dataIndex: 'secCostAllocateTypeID',
+            scopedSlots: { customRender: 'secCostAllocateTypeID' },
         },
         {
-            title: '分摊方式',
-            dataIndex: 'shareType',
-            scopedSlots: { customRender: 'shareType' },
-            width: '150px'
+            title: '总建筑面积(不含停车库)(CFA)',
+            dataIndex: 'totalCFAExcludeParking',
+            scopedSlots: { customRender: 'totalCFAExcludeParking' },
         },
         {
-            title: '总金额',
+            title: '总计容面积(GFA)',
+            dataIndex: 'totalGFA',
+            scopedSlots: { customRender: 'totalGFA' },
+        },
+        {
+            title: '比例',
+            dataIndex: 'rate',
+            scopedSlots: { customRender: 'rate' },
+        },
+        {
+            title: '分摊金额',
             dataIndex: 'allAmount',
             scopedSlots: { customRender: 'allAmount' },
-            width: '180px'
-        }
+        },
     ]
 
     const contractTypes = {
@@ -150,7 +199,12 @@
             this.columns = columns
             return {
                 // create model
-                data: { tableData: [] },
+                data: {
+                    tableData: [],
+                    itemType: null,
+                    shareType: null,
+                    allAmount: null
+                },
                 show: false,
                 visible: false,
                 confirmLoading: false,
@@ -158,9 +212,20 @@
                 tableRules: {
                     tableData: [],
                 },
+                rules: {
+                    itemType: [{ required: true, message: '请选择清单项类别', trigger: 'change' }],
+                    shareType: [{ required: true, message: '请选择分摊方式', trigger: 'change' }],
+                    allAmount: [{ required: true, message: '请输入总金额', trigger: 'change' }]
+                },
                 mdl: null,
                 // 高级搜索 展开/关闭
-                advanced: false
+                advanced: false,
+                itemType: null,
+                shareType: null,
+                allAmount: null,
+                selectedRowKeys: [],
+                selectedRows: [],
+                secCostAllocateTypes: null
             }
         },
         props: {
@@ -169,7 +234,19 @@
                 default: null
             }
         },
-        computed: {},
+        computed: {
+            rowSelection () {
+                const that = this
+                return {
+                    selectedRowKeys: this.selectedRowKeys,
+                    onChange: this.onSelectChange,
+                    type: 'checkbox',
+                    onSelect: function (record, selected, selectRows, nativeEvent) {
+
+                    }
+                }
+            },
+        },
         created () {
             if (this.contract.contract.contractCategory) {
                 const contractTypeKey = contractTypes[this.contract.contract.contractCategory + '']
@@ -182,28 +259,64 @@
             }
             ContractService.centers(this.contract.contract.tenderPackageItemID).then(res => {
                 this.selection.centers = res.result.data
+                this.selection.centers.forEach(item => {
+                    const param = {
+                        costCenter: item.id + '',
+                        costCenterName: item.costCenterName,
+                        secCostAllocateTypeID: item.secCostAllocateTypeID,
+                        totalCFAExcludeParking: item.totalCFAExcludeParking,
+                        totalGFA: item.totalGFA
+                    }
+                    this.data.tableData.push(param)
+                })
                 this.$forceUpdate()
             })
             BaseService.shareTypes().then(res => {
                 this.selection.shareTypes = res.result.data
                 this.$forceUpdate()
             })
+            CostService.secCostAllocateTypes().then(res => {
+                this.secCostAllocateTypes = res.result.data
+            })
         },
         methods: {
+            computeShareAmount () {
+                let list = []
+                this.selectedRows.forEach(item => {
+                    const params = {
+                        costCenter: item.costCenter,
+                        rate: item.rate / 100
+                    }
+                    list.push(params)
+                })
+                const body = [{
+                    projectTenderPackageGUID: this.contract.contract.tenderPackageItemID,
+                    costCenterIdlst: list,
+                    shareType: this.data.shareType,
+                    allAmount: this.data.allAmount,
+                    itemType: this.data.itemType
+                }]
+                ContractService.shareTool(body).then(res => {
+                    if (res.result.statusCode === 200) {
+                        res.result.data.forEach(item => {
+                            const index = this.data.tableData.findIndex(d => d.costCenter === item.costCenter)
+                            this.data.tableData[index].allAmount = item.allAmount
+                            this.$forceUpdate()
+                        })
+                    }
+                })
+            },
+            onSelectChange (selectedRowKeys, selectedRows) {
+                this.selectedRowKeys = selectedRowKeys
+                this.selectedRows = selectedRows
+            },
             showTable () {
                 this.visible = true
-                if (this.$refs.table) {
-                    this.data.tableData = []
-                }
             },
             handleCancel () {
                 this.visible = false
             },
             handleOk () {
-                if (this.data.tableData.length < 1) {
-                    this.$message.warn('请添加分摊信息')
-                    return
-                }
                 this.confirmLoading = true
                 let isValid = true
                 this.$refs.form.validate(valid => {
@@ -212,39 +325,21 @@
                     }
                 })
                 if (isValid) {
-                    ContractService.shareTool(this.data.tableData).then(res => {
-                        if (res.result.statusCode === 200) {
-                            res.result.data.forEach(item => {
-                                this.$parent.add(item)
-                            })
-                            this.confirmLoading = false
-                            this.visible = false
-                        }
-                    }).catch(e => {
-                        this.confirmLoading = false
-                        console.error('请求获取变更清单分摊数据错误', e)
+                    this.selectedRows.forEach(item => {
+                        item.itemType = this.data.itemType
+                        this.$parent.add(item)
                     })
+                    this.confirmLoading = false
+                    this.visible = false
                 } else {
                     this.confirmLoading = false
                 }
             },
-            del (index) {
-                this.data.tableData.splice(index, 1)
-            },
-            add () {
-                const param = {
-                    projectTenderPackageGUID: this.contract.contract.tenderPackageItemID
-                }
-                this.data.tableData.push(param)
-            }
         }
     }
 </script>
 
 <style lang="less" scoped>
-  /deep/ .ant-form-item-label label {
-    color: #fff;
-  }
 
   .ant-form .ant-form-item .ant-form-item-label.ant-col-8 {
     width: 15em;
